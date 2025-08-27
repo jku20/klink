992160b8732409776da2a1d53f02c9a17cabfe31
jeremy ku-benjet <jeremykubenjetschool@gmail.com>
2025-08-27 10:40:57 -0400

    Implement unit propagation.

--- figuring_stuff_out/lib/planner.ml
+++ figuring_stuff_out/lib/planner.ml
@@ -23,6 +23,11 @@ type decision =
   | Undecided
 [@@deriving sexp]
 
+type dependancy =
+  | Composite of state_id list list
+  | Primative
+[@@deriving sexp]
+
 type t =
   { solution : decision Map.M(String).t
     (* A list of clauses, each clause is a set of states which are interchangeable *)
@@ -30,13 +35,18 @@ type t =
   ; inputs : state_id list
   ; states : state list
   ; ops : op list
-  ; deps : state_id list list Map.M(String).t
+  ; deps : dependancy Map.M(String).t
   }
 [@@deriving sexp]
 
-let make inputs outputs states ops =
-  let solution = Map.empty (module String) in
-  let clauses = List.map ~f:(fun x -> [ x ]) outputs in
+let make inputs outputs (states : state list) ops =
+  let solution =
+    List.fold
+      inputs
+      ~init:(Map.empty (module String))
+      ~f:(fun acc s -> Map.set acc ~key:s ~data:Present)
+  in
+  let clauses = List.map ~f:(fun x -> [ [ x ] ]) outputs in
   let open List in
   let deps =
     fold
@@ -44,34 +54,72 @@ let make inputs outputs states ops =
       ~init:(Map.empty (module String))
       ~f:(fun acc op ->
         fold op.outputs ~init:acc ~f:(fun acc s ->
-          let new_deps =
+          let new_deps : state_id list list =
             match Map.find acc s with
-            | Some l -> op.inputs :: l
-            | None -> [ op.inputs ]
+            | Some (Composite l) -> op.inputs :: l
+            | _ -> [ op.inputs ]
           in
-          Map.set ~key:s ~data:new_deps acc))
+          Map.set ~key:s ~data:(Composite new_deps) acc))
+  in
+  let deps =
+    fold states ~init:deps ~f:(fun acc s ->
+      if Map.mem acc s.name then acc else Map.set acc ~key:s.name ~data:Primative)
   in
   { solution; clauses; inputs; states; ops; deps }
 ;;
 
 let unit_prop planner =
   let open List in
-  let forced_clauses =
-    []
-    (*
-       filter planner.clauses ~f:(fun l -> length l = 1)
-            |> fold clauses ~init:[] ~f:(fun acc c ->
-                match c with 
-                | [] -> assert false 
-                | [ [ s ] ] -> if mem.planner.inputs h ~equal:String.equal then h else match Map.find planner.deps h with None -> assert false | (Some d -> match d with [ l ] -> acc @ l | _ -> acc)
-                | 
-        )
-    *)
+  (* get new states *)
+  let new_states_from_only_option =
+    filter planner.clauses ~f:(fun l -> length l = 1)
+    |> map ~f:(function
+      | [ l ] -> l
+      | _ -> assert false)
+    |> fold ~init:[] ~f:(fun acc l -> l @ acc)
   in
-  let new_clauses =
-    fold forced_states ~init:planner.clauses ~f:(fun acc s ->
-      map acc ~f:(fun l -> filter l ~f:(Fun.negate (String.equal s))))
-    |> filter ~f:(Fun.negate is_empty)
+  (* get new deps from those states *)
+  let cur_chosen_states =
+    Map.filter planner.solution ~f:(function
+      | Present -> true
+      | _ -> false)
+    |> Map.keys
   in
-  { planner with clauses = new_clauses }
+  let new_deps =
+    let open Result in
+    let can_make_primitive s = mem cur_chosen_states s ~equal:String.equal in
+    fold new_states_from_only_option ~init:(Ok []) ~f:(fun acc s ->
+      match acc with
+      | Error msg -> if can_make_primitive s then Error msg else Error (msg ^ " " ^ s)
+      | Ok a ->
+        (match Map.find_exn planner.deps s with
+         | Composite c -> Ok (c :: a)
+         | Primative ->
+           if can_make_primitive s
+           then Ok a
+           else Error ("Cannot make the following states: " ^ s)))
+  in
+  Result.bind new_deps ~f:(fun new_deps ->
+    (* add those deps to the clauses *)
+    let cur_chosen_states =
+      Map.filter planner.solution ~f:(function
+        | Present -> true
+        | _ -> false)
+      |> Map.keys
+    in
+    (* prune the new clauses for states in the solution *)
+    let new_solution = new_states_from_only_option @ cur_chosen_states in
+    let new_clauses =
+      new_deps @ planner.clauses
+      |> map ~f:(fun l ->
+        map l ~f:(fun t ->
+          filter t ~f:(Fun.negate (mem new_solution ~equal:String.equal))))
+      |> filter ~f:(fun l -> for_all l ~f:(Fun.negate is_empty))
+      |> filter ~f:(Fun.negate is_empty)
+    in
+    let new_solution =
+      fold new_solution ~init:planner.solution ~f:(fun acc s ->
+        Map.set acc ~key:s ~data:Present)
+    in
+    Ok { planner with clauses = new_clauses; solution = new_solution })
 ;;
--- figuring_stuff_out/test/test_planner.ml
+++ figuring_stuff_out/test/test_planner.ml
@@ -4,6 +4,10 @@ let test_planner planner =
   Planner.sexp_of_t planner |> Sexp.to_string_hum |> print_endline
 ;;
 
+let test_unit_prop_planner planner =
+  [%sexp_of: (Planner.t, string) Result.t] planner |> Sexp.to_string_hum |> print_endline
+;;
+
 let sa : Planner.state = { name = "a"; param = [] }
 let sb : Planner.state = { name = "b"; param = [] }
 let sc : Planner.state = { name = "c"; param = [] }
@@ -11,18 +15,99 @@ let sd : Planner.state = { name = "d"; param = [] }
 let op1 : Planner.op = { name = "op1"; inputs = [ "a"; "b" ]; outputs = [ "c" ] }
 let op2 : Planner.op = { name = "op2"; inputs = [ "a"; "c" ]; outputs = [ "d" ] }
 let op3 : Planner.op = { name = "op3"; inputs = [ "b"; "c" ]; outputs = [ "d" ] }
-let planner_unit_prop = Planner.make [] ["c"] [ sa; sb; sc; sd ] [ op1; op2; op3 ]
 
 let%expect_test "make" =
   let new_planner =
     Planner.make [ "a"; "b" ] [ "d"; "c" ] [ sa; sb; sc; sd ] [ op1; op2; op3 ]
   in
   test_planner new_planner;
-  [%expect {| |}]
+  [%expect
+    {|
+    ((solution ((a Present) (b Present))) (clauses (((d)) ((c)))) (inputs (a b))
+     (states
+      (((name a) (param ())) ((name b) (param ())) ((name c) (param ()))
+       ((name d) (param ()))))
+     (ops
+      (((name op1) (inputs (a b)) (outputs (c)))
+       ((name op2) (inputs (a c)) (outputs (d)))
+       ((name op3) (inputs (b c)) (outputs (d)))))
+     (deps
+      ((a Primative) (b Primative) (c (Composite ((a b))))
+       (d (Composite ((b c) (a c)))))))
+    |}]
 ;;
 
-let%expect_test "unit_prop" =
-  let propped = Planner.unit_prop planner_unit_prop in
-  test_planner propped;
-  [%expect {| ((solution (a)) (clauses ((b))) (inputs ()) (states (a b)) (ops ())) |}]
+let%expect_test "unit_prop simple remaining clauses" =
+  let planner = Planner.make [] [ "c" ] [ sa; sb; sc; sd ] [ op1; op2; op3 ] in
+  let propped = Planner.unit_prop planner in
+  test_unit_prop_planner propped;
+  [%expect
+    {|
+    (Ok
+     ((solution ((c Present))) (clauses (((a b)))) (inputs ())
+      (states
+       (((name a) (param ())) ((name b) (param ())) ((name c) (param ()))
+        ((name d) (param ()))))
+      (ops
+       (((name op1) (inputs (a b)) (outputs (c)))
+        ((name op2) (inputs (a c)) (outputs (d)))
+        ((name op3) (inputs (b c)) (outputs (d)))))
+      (deps
+       ((a Primative) (b Primative) (c (Composite ((a b))))
+        (d (Composite ((b c) (a c))))))))
+    |}]
+;;
+
+let%expect_test "unit_prop simple no remaining clauses" =
+  let planner = Planner.make [ "a"; "b" ] [ "c" ] [ sa; sb; sc; sd ] [ op1; op2; op3 ] in
+  let propped = Planner.unit_prop planner in
+  test_unit_prop_planner propped;
+  [%expect
+    {|
+    (Ok
+     ((solution ((a Present) (b Present) (c Present))) (clauses ())
+      (inputs (a b))
+      (states
+       (((name a) (param ())) ((name b) (param ())) ((name c) (param ()))
+        ((name d) (param ()))))
+      (ops
+       (((name op1) (inputs (a b)) (outputs (c)))
+        ((name op2) (inputs (a c)) (outputs (d)))
+        ((name op3) (inputs (b c)) (outputs (d)))))
+      (deps
+       ((a Primative) (b Primative) (c (Composite ((a b))))
+        (d (Composite ((b c) (a c))))))))
+    |}]
+;;
+
+let%expect_test "unit_prop idempotent" =
+  let open Result in
+  let planner = Planner.make [ "a"; "b" ] [ "c" ] [ sa; sb; sc; sd ] [ op1; op2; op3 ] in
+  let propped_once = Planner.unit_prop planner in
+  let propped_twice = propped_once >>= Planner.unit_prop in
+  test_unit_prop_planner propped_twice;
+  [%expect {|
+    (Ok
+     ((solution ((a Present) (b Present) (c Present))) (clauses ())
+      (inputs (a b))
+      (states
+       (((name a) (param ())) ((name b) (param ())) ((name c) (param ()))
+        ((name d) (param ()))))
+      (ops
+       (((name op1) (inputs (a b)) (outputs (c)))
+        ((name op2) (inputs (a c)) (outputs (d)))
+        ((name op3) (inputs (b c)) (outputs (d)))))
+      (deps
+       ((a Primative) (b Primative) (c (Composite ((a b))))
+        (d (Composite ((b c) (a c))))))))
+    |}]
+;;
+
+let%expect_test "unit_prop can't make state" =
+  let open Result in
+  let planner = Planner.make [] [ "c" ] [ sa; sb; sc; sd ] [ op1; op2; op3 ] in
+  let propped_once = Planner.unit_prop planner in
+  let propped_twice = propped_once >>= Planner.unit_prop in
+  test_unit_prop_planner propped_twice;
+  [%expect {| (Error "Cannot make the following states: a b") |}]
 ;;
